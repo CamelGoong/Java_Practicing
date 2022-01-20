cb5989f21857eea393e63d490d3921f22e8ad467
CamelGoong <goong0405@gmail.com>
2022-01-21 00:06:40 +0900

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		// 변수를 만들 때, 변수 안에 어떤 데이터 타입이 들어갈 수 있는지를 지정해주어야 함.
+		
+		int a = 1; // Integer: 정수. 소수점이 없는 숫자
+		System.out.println(a);
+		
+		double b = 1.1; // Double: 실수. real number. 소수점이 있는 숫자
+		System.out.println(b);
+		 
+		String c = "Hello World"; // S 대문자로.
+		System.out.println(c);
+		 
+	}
+
+}
