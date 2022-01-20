f56f2883057e6066c719208c86671ab09710f1a1
CamelGoong <goong0405@gmail.com>
2022-01-21 00:11:53 +0900

    Add files via upload

---
+++ Letter.java
@@ -0,0 +1,14 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		
+		String name = "egoing";
+		System.out.println("Hello, "+ name + "..." + name + "..." + "bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+		
+	}
+
+}
