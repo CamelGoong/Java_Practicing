d0c3944eaadda9fe1ad53dba684d698b8f60b313
CamelGoong <goong0405@gmail.com>
2022-01-20 23:38:07 +0900

    Add files via upload

---
+++ StringOperation.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, haegyun ... bye.".replace("haegyun", "taewon")); // replace(a,b) : a¸¦ b·Î ¹Ù²Þ
+		
+	}
+
+}
