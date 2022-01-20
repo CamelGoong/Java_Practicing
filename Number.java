a6edb5ea3f4bf26dd639a1210ecc8a4b64d6258b
CamelGoong <goong0405@gmail.com>
2022-01-20 23:27:03 +0900

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,16 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		System.out.println(Math.PI); //3.14
+		System.out.println(Math.floor(Math.PI)); // floor: 내림, 소수점을 자름.
+		System.out.println(Math.ceil(Math.PI)); // ceil: 올림
+	}
+
+}
---
+++ StringApp.java
@@ -0,0 +1,16 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String(character들이 모여있는 데이터타입) 큰따옴표
+		System.out.println('H'); // Character(한글자를 표현하는 데이터타입) 작은따옴표
+		System.out.println("H"); // 이건 큰따옴표니까 String
+		
+		// \n: new line
+		System.out.println("Hello \nWorld");
+		
+		System.out.println("Hello \"World\""); // Hello "World" 역슬래쉬 다음에 보이고 싶은 문자기호를 넣으면 그렇게 처리가 됨
+	}
+
+}
